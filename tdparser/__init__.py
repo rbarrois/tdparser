78793e5400f110d2ff0f10e30b6431682d112fad
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-08-17 15:52:05 +0200

    Expose common classes in tdparser.__init__.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

---
+++ tdparser/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+__version__ = '0.1.0'
+__author__ = u"Raphaël Barrois <raphael.barrois@polytechnique.org>"
+
+
+from .parser import (
+    Token, EndToken,
+    LeftParen, RightParen,
+
+    Parser,
+    Lexer,
+)
+
