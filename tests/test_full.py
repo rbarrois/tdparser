49fe8cf0784cb602dec70c3033dd98401c33028d
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-08-22 00:54:37 +0200

    Add arithmetic parser tests.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- tests/test_full.py
+++ tests/test_full.py
@@ -10,7 +10,84 @@ import unittest
 import tdparser
 
 
-class ParenthesizedLexerTestCase(unittest.TestCase):
+class ArithmeticParserTestCase(unittest.TestCase):
+    """Test parsing arithmetic expressions."""
+    def setUp(self):
+
+        class Integer(tdparser.Token):
+            """A simple integer."""
+            def __init__(self, text):
+                super(Integer, self).__init__(text)
+                self.value = int(text)
+
+            def nud(self, context):
+                """It evaluates to its (integer) value."""
+                return self.value
+
+        class Add(tdparser.Token):
+            """Addition."""
+
+            lbp = 10  # Precedence: higher than integers, lower than mult
+
+            def led(self, left, context):
+                """Evaluates to "left + <on the right up to next mult>"."""
+                return left + context.expression(self.lbp)
+
+        class Minus(tdparser.Token):
+            lbp = 10
+
+            def nud(self, context):
+                """At the beginning of an expression, negates the expression."""
+                return - context.expression(self.lbp)
+
+            def led(self, left, context):
+                """In the middle of an expression, computes "left - right"."""
+                return left - context.expression(self.lbp)
+
+        class Mult(tdparser.Token):
+            lbp = 20  # Higher precedence than addition/substraction.
+
+            def led(self, left, context):
+                return left * context.expression(self.lbp)
+
+        l = tdparser.Lexer(default_tokens=True)
+        l.register_token(Integer, re.compile(r'[0-9]+'))
+        l.register_token(Add, re.compile(r'\+'))
+        l.register_token(Minus, re.compile(r'-'))
+        l.register_token(Mult, re.compile(r'\*'))
+
+        self.lexer = l
+
+    def test_single_number(self):
+        val = self.lexer.parse('13')
+        self.assertEqual(13, val)
+
+    def test_negative_number(self):
+        val = self.lexer.parse('-13')
+        self.assertEqual(-13, val)
+        self.assertEqual(13, self.lexer.parse('--13'))
+        self.assertEqual(13, self.lexer.parse('----13'))
+
+    def test_simple_mult(self):
+        self.assertEqual(8, self.lexer.parse('2 * 4'))
+        self.assertEqual(8, self.lexer.parse('2 * 2 * 2'))
+        self.assertEqual(8, self.lexer.parse('2 * (2 * 2)'))
+        self.assertEqual(8, self.lexer.parse('(2 * 2) * 2'))
+        self.assertEqual(8, self.lexer.parse('(2 + 2) * 2'))
+
+    def test_precedence(self):
+        self.assertEqual(8, self.lexer.parse('2 * 3 + 2'))
+        self.assertEqual(10, self.lexer.parse('2 * (3+2)'))
+
+    def test_negative_mult(self):
+        self.assertEqual(8, self.lexer.parse('-2 * - 4'))
+        self.assertEqual(8, self.lexer.parse('-2 * - (2 + 2)'))
+        self.assertEqual(8, self.lexer.parse('-2 * 2 * -2'))
+        self.assertEqual(8, self.lexer.parse('--2 * -2 * -2'))
+        self.assertEqual(8, self.lexer.parse('-(-2 * -2 * -2)'))
+
+
+class ParenthesizedParserTestCase(unittest.TestCase):
     """Test lexing parenthesized expressions."""
 
     def setUp(self):
